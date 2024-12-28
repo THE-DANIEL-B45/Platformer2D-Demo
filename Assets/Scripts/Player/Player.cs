74dc2d9e85d0b924b544ade8afdaada89e36ad3e
THE-DANIEL-B45 <danielborgeslazzarini0@gmail.com>
2024-12-28 18:59:49 -0300

    Add Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(0.1f, 0);
 
     public float speed;
 
+    public float jumpForce = 2.0f;
+
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(+speed, myRigidBody.velocity.y);
+        }
+
+        if(myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity -= friction;
+        }
+        else if(myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = Vector2.up * jumpForce;
         }
     }
 }
