310b48bb67ddf2aae4a98405e7a5c16b2782d827
THE-DANIEL-B45 <danielborgeslazzarini0@gmail.com>
2024-12-30 18:26:03 -0300

    Contabilizando moedas na interface

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,10 +1,13 @@
 using Ebac.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemManager : Singleton<ItemManager>
 {
+    public TextMeshProUGUI coinsText;
+
     public int coins;
 
     private void Start()
@@ -15,10 +18,12 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        coinsText.text = "x0";
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        coinsText.text = "x" + coins.ToString();
     }
 }
